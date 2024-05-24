98f9a42f629290000bcee7a9ee1bd268bd9d058c
AlexanderClausen <git@alexanderclausen.com>
2024-05-24 20:10:27 +1000

    detail page layout (wip)

--- cardetails.php
+++ cardetails.php
@@ -28,8 +28,8 @@ if ($_GET['id'] == '') {
                 }
             echo '</div>';
         echo '</div>';
-        echo '<p class="chip ' . ($car->booked ? 'unavailable' : 'available') .'" id="car-availability">' . ($car->booked ? 'Booked' : 'Available') . '</p>';
-        echo '<p id="car-type" class="smalltext">' . $car->type . '</p>';
+        // echo '<p class="chip ' . ($car->booked ? 'unavailable' : 'available') .'" id="car-availability">' . ($car->booked ? 'Booked' : 'Available') . '</p>';
+        echo '<p id="car-type" class="smalltext">' . $car->class . ' car | ' . $car->type . '</p>';
         // echo '<p id="car-name" class="bigtext">' . $car->name . ' (' . $car->year . ')</p>';
         echo '<div class="container-spacebetween">';
             echo '<p id="car-model" class="bigtext">' . $car->name . ' (' . $car->year . ')</p>';
@@ -38,21 +38,43 @@ if ($_GET['id'] == '') {
                 echo '<span class="smalltext"> / day</span>';
             echo '</div>';
         echo '</div>';
-        
-        echo '<p id="car-infoline">' . $car->class . ' car</p>';
-        echo '<p id="car-description">' . $car->description . '</p>';
 
-        echo '<table><tr>';
-            echo '<td><p class="feature ' . ($car->features->aircon ? 'true' : 'false') . ' tooltip" id="car-aircon"><span class="material-symbols-outlined">mode_fan</span><span class="tooltiptext">A/C: ' . ($car->features->aircon ? '&check;' : '&cross;') . '</span></p></td>';
-            echo '<td><p class="feature ' . ($car->features->bluetooth ? 'true' : 'false') . ' tooltip" id="car-bluetooth"><span class="material-symbols-outlined">bluetooth</span><span class="tooltiptext">Bluetooth: ' . ($car->features->bluetooth ? '&check;' : '&cross;') . '</span></p></td>';
-            echo '<td><p class="feature ' . ($car->features->carplay ? 'true' : 'false') . ' tooltip" id="car-carplay"><span class="material-symbols-outlined">ios</span><span class="tooltiptext">Apple CarPlay: ' . ($car->features->carplay ? '&check;' : '&cross;') . '</span></p></td>';
-            echo '<td><p class="feature ' . ($car->features->androidauto ? 'true' : 'false') . ' tooltip" id="car-androidauto"><span class="material-symbols-outlined">android</span><span class="tooltiptext">Android Auto: ' . ($car->features->androidauto ? '&check;' : '&cross;') . '</span></p></td>';
-            echo '<td><p class="feature ' . ($car->features->gps ? 'true' : 'false') . ' tooltip" id="car-gps"><span class="material-symbols-outlined">near_me</span><span class="tooltiptext">GPS: ' . ($car->features->gps ? '&check;' : '&cross;') . '</span></p></td>';
-            echo '<td><p class="feature ' . ($car->features->parkingassistant ? 'true' : 'false') . ' tooltip" id="car-parkingassistant"><span class="material-symbols-outlined">local_parking</span><span class="tooltiptext">Parking assistant: ' . ($car->features->parkingassistant ? '&check;' : '&cross;') . '</span></p></td>';
-            echo '<td><p class="feature ' . ($car->features->cruisecontrol ? 'true' : 'false') . ' tooltip" id="car-cruisecontrol"><span class="material-symbols-outlined">speed</span><span class="tooltiptext">Cruise Control: ' . ($car->features->cruisecontrol ? '&check;' : '&cross;') . '</span></p></td>';
-        echo '</tr></table>';
+        echo '<hr>';
 
-        echo '<p id="car-price">$' . number_format($car->price, 2) . '/day</p>';
-        echo '<button>Book</button>';
+        echo '<div class="container">';
+            echo '<div id="car-properties-container">';
+                echo '<div class="container-spacebetween">';
+                    echo '<span class="feature ' . ($car->features->aircon ? 'true' : 'false') . ' tooltip" id="car-aircon"><span class="material-symbols-outlined">mode_fan</span><span class="tooltiptext">A/C: ' . ($car->features->aircon ? '&check;' : '&cross;') . '</span></span>';
+                    echo '<span class="feature ' . ($car->features->bluetooth ? 'true' : 'false') . ' tooltip" id="car-bluetooth"><span class="material-symbols-outlined">bluetooth</span><span class="tooltiptext">Bluetooth: ' . ($car->features->bluetooth ? '&check;' : '&cross;') . '</span></span>';
+                    echo '<span class="feature ' . ($car->features->carplay ? 'true' : 'false') . ' tooltip" id="car-carplay"><span class="material-symbols-outlined">ios</span><span class="tooltiptext">Apple CarPlay: ' . ($car->features->carplay ? '&check;' : '&cross;') . '</span></span>';
+                    echo '<span class="feature ' . ($car->features->androidauto ? 'true' : 'false') . ' tooltip" id="car-androidauto"><span class="material-symbols-outlined">android</span><span class="tooltiptext">Android Auto: ' . ($car->features->androidauto ? '&check;' : '&cross;') . '</span></span>';
+                    echo '<span class="feature ' . ($car->features->gps ? 'true' : 'false') . ' tooltip" id="car-gps"><span class="material-symbols-outlined">near_me</span><span class="tooltiptext">GPS: ' . ($car->features->gps ? '&check;' : '&cross;') . '</span></span>';
+                    echo '<span class="feature ' . ($car->features->parkingassistant ? 'true' : 'false') . ' tooltip" id="car-parkingassistant"><span class="material-symbols-outlined">local_parking</span><span class="tooltiptext">Parking assistant: ' . ($car->features->parkingassistant ? '&check;' : '&cross;') . '</span></span>';
+                    echo '<span class="feature ' . ($car->features->cruisecontrol ? 'true' : 'false') . ' tooltip" id="car-cruisecontrol"><span class="material-symbols-outlined">speed</span><span class="tooltiptext">Cruise Control: ' . ($car->features->cruisecontrol ? '&check;' : '&cross;') . '</span></span>';
+                echo '</div>';
+                echo '<table id="car-properties" class="flex">';
+                    echo '<tr>';
+                        echo '<td><span class="material-symbols-outlined">auto_transmission</span><span>Transmission:</span></td>';
+                        echo '<td>' . $car->transmission . '</td>';
+                    echo '</tr>';
+                    echo '<tr>';
+                        echo '<td><span class="material-symbols-outlined">local_gas_station</span><span>Fuel type:</span></td>';
+                        echo '<td>' . $car->fuel . '</td>';
+                    echo '</tr>';
+                    echo '<tr>';
+                        echo '<td><span class="material-symbols-outlined">group</span><span>Seats:</span></td>';
+                        echo '<td>' . $car->seats . ' seats</td>';
+                    echo '</tr>';
+                    echo '<tr>';
+                        echo '<td><span class="material-symbols-outlined">luggage</span><span>Luggage:</span></td>';
+                        echo '<td>' . $car->luggage . ' bags</td>';
+                    echo '</tr>';
+                echo '</table>';
+            echo '</div>';
+            echo '<p id="car-description" class="flex">' . $car->description . '</p>';
+        echo '</div>';
+
+        // echo '<p id="car-price">$' . number_format($car->price, 2) . '/day</p>';
+        // echo '<button>Book</button>';
     echo '</div>';
 }
